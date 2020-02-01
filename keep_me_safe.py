96d045ae9e6f39be77d5706153c9c570dfc2f3db
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2020-01-31 21:55:20 -0600

    add send email test, use unittest and vcrpy

--- keep_me_safe.py
+++ keep_me_safe.py
@@ -12,29 +12,36 @@ class ParameterNotFound(Exception):
     pass
 
 
-def get_env_variable(parameter_name):
-    if not parameter_name or not isinstance(parameter_name, type('')):
-        return
+def get_env_variable(parameter_name: str) -> str:
+    if not parameter_name or not isinstance(parameter_name, str):
+        return ''
     result = os.environ.get(parameter_name)
     if not result:
         raise ParameterNotFound(parameter_name)
     return result
 
 
-traffic_page = requests.get('https://lb.511ia.org/ialb/cameras/camera.jsf;jsessionid=CDRUfIHRjFD3g-KsTsIcu5xggMRZMM7pZrCqWiUt.ip-10-4-73-18?id=59169258&view=state&text=m&textOnly=false')
-soup = BeautifulSoup(traffic_page.content)
-img_url = soup.find(id='cam-0-img')['src']
+def do_the_thing() -> None:
+    traffic_page = requests.get(
+        'https://lb.511ia.org/ialb/cameras/camera.jsf;jsessionid=CDRUfIHRjFD3g-KsTsIcu5xggMRZMM7pZrCqWiUt.ip-10-4-73-18?id=59169258&view=state&text=m&textOnly=false'
+    )
+    soup = BeautifulSoup(traffic_page.content)
+    img_url = soup.find(id='cam-0-img')['src']
 
-message = Mail(
-    from_email='keep_me_safe@me_so_safe.com',
-    to_emails=get_env_variable('DESTINATION_EMAIL'),
-    subject='Traffic Conditions Today',
-    html_content='<img src={}>'.format(img_url)
-)
-sg = SendGridAPIClient(get_env_variable('SENDGRID_API_KEY'))
+    message = Mail(
+        from_email='keep_me_safe@me_so_safe.com',
+        to_emails=get_env_variable('DESTINATION_EMAIL'),
+        subject='Traffic Conditions Today',
+        html_content='<img src={}>'.format(img_url)
+    )
+    sg = SendGridAPIClient(get_env_variable('SENDGRID_API_KEY'))
 
-response = sg.send(message)
+    response = sg.send(message)
 
-print(response.status_code)
-print(response.body)
-print(response.headers)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+
+if "__main__" == __name__:
+    do_the_thing()
--- test_keep_me_safe.py
+++ test_keep_me_safe.py
@@ -1,26 +1,47 @@
 import os
-import pytest
+import unittest
+from python_http_client import UnauthorizedError
 
-from keep_me_safe import get_env_variable, ParameterNotFound
+from keep_me_safe import do_the_thing, get_env_variable, ParameterNotFound
+import vcr
+
+my_vcr = vcr.VCR(
+    serializer='yaml',
+    cassette_library_dir='fixtures/cassettes',
+    record_mode='once',
+    match_on=['uri', 'method'],
+)
 
 
-class TestKeepMeSafe:
+class TestKeepMeSafe(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        try:
+            del os.environ['abcdefg']
+        except KeyError:
+            pass
 
     def test_raises_parmeter_not_found_when_no_destination_email(self):
         parameter_name = 'abcdefg'
-        with pytest.raises(ParameterNotFound):
+        with self.assertRaises(ParameterNotFound):
             get_env_variable(parameter_name)
 
     def test_gets_parameter_when_set(self):
         parameter_name = 'abcdefg'
         expected_parameter = 'test_variable'
         os.environ[parameter_name] = expected_parameter
-        assert expected_parameter == get_env_variable(parameter_name)
+        self.assertEqual(expected_parameter, get_env_variable(parameter_name))
 
     def test_returns_none_when_parameter_name_is_wrong_type(self):
         parameter_name = []
-        assert None is get_env_variable(parameter_name)
-        parameter_name = {}
-        assert None is get_env_variable(parameter_name)
-        parameter_name = 1
-        assert None is get_env_variable(parameter_name)
+        self.assertEqual('', get_env_variable(parameter_name))
+
+    @my_vcr.use_cassette
+    def test_sends_email_throws_unauthorized_when_bad_api_key(self):
+        os.environ['DESTINATION_EMAIL'] = 'test@example.com'
+        os.environ['SENDGRID_API_KEY'] = 'test_api_key'
+        with self.assertRaises(UnauthorizedError):
+            do_the_thing()
